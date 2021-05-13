9bb8c14adeb6fe8c077ee7f82c61f7eb52960687
liujianfengv <27178254@qq.com>
2021-05-14 00:07:09 +0800

    finish  delete operation in BTree

--- DataStructure/B-tree/BTree.cpp
+++ DataStructure/B-tree/BTree.cpp
@@ -223,3 +223,201 @@ void TreeNode::removeFromLeaf(int idx)
     n--;
     return;
 }
+
+void TreeNode::removeFromNonLeaf(int idx)
+{
+    int k = keys[idx];
+
+    // If the child that precedes k (C[idx]) has atleast t keys,
+    // find the predecessor 'pred' of k in the subtree rooted at
+    // C[idx]. Replace k by pred. Recursively delete pred in C[idx]
+    if (C[idx]->n >= t) {
+        int pred = getPred(idx);
+        keys[idx] = pred;
+        C[idx]->remove(pred);
+    }
+
+    // If the child C[idx] has less that t keys, examine C[idx+1].
+    // If C[idx+1] has atleast t keys, find the successor 'succ' of k in
+    // the subtree rooted at C[idx+1]
+    // Replace k by succ
+    // Recursively delete succ in C[idx+1]
+    else if (C[idx+1]->n >= t) {
+        int succ = getSucc(idx);
+        keys[idx] = succ;
+        C[idx + 1]->remove(succ);
+    }
+
+    // If both C[idx] and C[idx+1] has less than t keys, merge k and all of C[idx+1]
+    // into C[idx]
+    // Now C[idx] contains 2t-1 keys
+    // Free C[idx+1] and recursively delete k from C[idx]
+    else {
+        merge(idx);
+        C[idx]->remove(k);
+    }
+    return;
+}
+
+int TreeNode::getPred(int idx)
+{
+    // Keep moving to the right most node until we reach a leaf
+    TreeNode *cur = C[idx];
+    while (!cur->leaf) {
+        cur = cur->C[cur->n];
+    }
+
+    // Return the last key of the leaf
+    return cur->keys[cur->n - 1];
+
+}
+
+int TreeNode::getSucc(int idx)
+{
+    // Keep moving the left most node starting from C[idx+1] until we reach
+    // a leaf
+    TreeNode *cur = C[idx + 1];
+    while (!cur->leaf) {
+        cur = cur->C[0];
+    }
+    return cur->keys[0];
+}
+
+void TreeNode::fill(int idx)
+{
+    // If the pervious child(C[idx-1]) has more than t-1 keys, borrow a key
+    // from that child
+    if (idx != 0 && C[idx - 1]->n >= t){
+        borrowFromPrev(idx);
+    }
+    // If the next child(C[idx+1]) has more than t-1 keys, borrow a key
+    // from that child
+    else if (idx != n && C[idx + 1]->n >= t){
+        borrowFromNext(idx);
+    }
+
+    // Merge C[idx] with its sibling
+    // If C[idx] is the last child, merge it with its previous sibling
+    // Otherwise merge it with its next sibling
+    else {
+        if (idx != n) {
+            merge(idx);
+        } else {
+            merge(idx - 1);
+        }
+    }
+    return;
+}
+
+void TreeNode::borrowFromPrev(int idx)
+{
+    TreeNode *child = C[idx];
+    TreeNode *sibling = C[idx - 1];
+
+    // The last key from C[idx - 1] goes up to the parent and key[idx-1]
+    // from parent is inserted as th first key in C[idx]. Thus, the loses
+    // sibling one key and child gains one key
+
+    // Moving all key in C[idx] one step ahead
+    for (int i = child->n-1; i >= 0; --i){
+        child->keys[i + 1] = child->keys[i];
+    }
+
+    // If C[idx] is not a leaf, move all its pointers one step ahead
+    if (!child->leaf) {
+        for(int i = child->n; i >= 0; --i){
+            child->C[i + 1] = child->C[i];
+        }
+    }
+
+    // Setting child's first key equal to keys[idx-1] from the current node
+    child->keys[0] = keys[idx - 1];
+
+    // Moving the key from the sibling to the parent
+    // This reduces the number of keys in the sibling
+    keys[idx - 1] = sibling->keys[sibling->n - 1];
+
+    child->n += 1;
+    sibling->n -= 1;
+
+    return;
+}
+
+void TreeNode::borrowFromNext(int idx)
+{
+    TreeNode *child = C[idx];
+    TreeNode *sibling = C[idx + 1];
+
+    // keys[idx] is inserted as the last key in C[idx]
+    child->keys[(child->n)] = keys[idx];
+
+    // Sibling's first child is inserted as the last child
+    // into C[idx]
+    if (!(child->leaf)) {
+        child->C[(child->n) + 1] = sibling->C[0];
+    }
+
+    // The first key from sibling is inserted into keys[idx]
+    keys[idx] = sibling->keys[0];
+
+    // Moving all keys in sibling one step behind
+    for (int i = 1; i < sibling->n; ++i) {
+        sibling->keys[i - 1] = sibling->keys[i];
+    }
+
+    // Moving the child pointers one step behind
+    if (!sibling->leaf) {
+        for (int i = 1; i <= sibling->n; ++i){
+            sibling->C[i - 1] = sibling->C[i];
+        }
+    }
+
+    // Increasing and decreadsing the key count of C[idx] and C[idx+1
+    // respectively
+
+    child->n += 1;
+    sibling->n -= 1;
+
+    return;
+}
+
+void TreeNode::merge(int idx)
+{
+    TreeNode *child = C[idx];
+    TreeNode *sibling = C[idx + 1];
+    // Pulling a key from the current node and inserting it into (t-1)th
+    // position of C[idx]
+    child->keys[t - 1] = keys[idx];
+
+    // Copying the keys from C[idx+1] to C[idx] at the end
+    for (int i = 0; i < sibling->n; ++i) {
+        child->keys[i + t] = sibling->keys[i];
+    }
+
+    // Copying the child pointer from C[idx+1] to C[idx]
+    if (!child->leaf) {
+        for (int i = 0; i <= sibling->n; ++i) {
+            child->C[i + t] = sibling->C[i];
+        }
+    }
+
+    // Moving all keys after idx in the current node one step before
+    // to fill the gap created by moving keys[idx] to C[idx]
+    for (int i = idx + 1; i < n; ++i) {
+        keys[i - 1] = keys[i];
+    }
+
+    // Moving the child pointers after (idx+1) in the current node on step
+    // before
+    for (int i = idx + 2; i < n; ++i) {
+        C[i - 1] = C[i];
+    }
+
+    // Updating the key count of child and the current node
+    child->n += sibling->n + 1;
+    n--;
+
+    // Freeing the memory occupied by sibling
+    delete(sibling);
+    return;
+}
--- DataStructure/B-tree/BTree.h
+++ DataStructure/B-tree/BTree.h
@@ -16,6 +16,8 @@ public:
     //Function to search key k in subtree rooted with this node
     TreeNode * search(int k); //returns NULL if k is not present
 
+    // A function that returns the index of the first key that is greater or
+    // equal to k
     int findKey(int k);
 
     // A utility funtion to insert a new key in this node
@@ -27,20 +29,39 @@ public:
     // Note that y must be full when this function is called
     void splitChild(int i, TreeNode *y);
 
+    // A wrapper function to remove the key k in subtree rooted with this node.
     void remove(int k);
 
+    // A function to remove the key persent in idx-th postion in this node
+    // this node which is a leaf
     void removeFromLeaf(int idx);
 
+    // A function to remove the key present in idx-th position in this node which is
+    // a non-leaf node
     void removeFromNonLeaf(int idx);
 
+    // A function to get the predecessor of the key where the key is
+    // present in the idx-th positon in the node
     int getPred(int idx);
 
+    // A function to get the successor of the key where the key is present
+    // in the idx-th position in the node
     int getSucc(int idx);
 
+    // A funtion to fill up the child node present in the idx-th position
+    // in the C[] array if that child has less than t-1 keys
     void fill(int idx);
 
+    // A function to borrow a key from the C[idx-1]-th node and place it
+    // in C[idx]th node
     void borrowFromPrev(int idx);
 
+    // A function to borrow a key from the C[idx+1]-th node and place it
+    // in C[idx]th node
+    void borrowFromNext(int idx);
+
+    // A function to merge idx-th child of the node with (idx+1)th child
+    // of the node
     void merge(int idx);
 
 
