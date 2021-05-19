774e728344ebd318c1875d1f0f65de179bf3370d
liujianfengv <27178254@qq.com>
2021-05-19 22:40:00 +0800

    finish mutilang demo

--- MutiLang/mainwindow.cpp
+++ MutiLang/mainwindow.cpp
@@ -1,15 +1,42 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QTranslator>
+#include <QDebug>
+QTranslator translator;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    if (!translator.load("debug\\zh_cn.qm")) {
+        qDebug()<<"load qm file error";
+        return;
+    }
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    qApp->installTranslator(&translator);
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    qApp->removeTranslator(&translator);
+}
+
+void MainWindow::changeEvent(QEvent *event)
+{
+    if (event->type() == QEvent::LanguageChange) {
+        ui->retranslateUi(this);
+        return;
+    }
+    QWidget::changeEvent(event);
 }
 
---
+++ MutiLang/zh_cn.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="52"/>
+        <source>Hello Qt!</source>
+        <translation>你好 QT!</translation>
+    </message>
+</context>
+</TS>
